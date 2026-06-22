22b3717bef6b6c5a696640f1a3dd6dc5f8e328f1
Satvik1462 <satvikbajpai503@gmail.com>
2026-06-22 14:28:24 +0530

    Mongo Atlas setup

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const userRoutes = require("./routes/userRoute");
 
 const app = express();
 
-
 app.use(cors());
 app.use(express.json());
 
+app.use("/users", userRoutes);
 
-app.use('/users', userRoutes);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Atlas Connected");
+  })
+  .catch((err) => {
+    console.log("MongoDB Error:", err);
+  });
 
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost:27017/placecom')
-.then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch((err) => {
-    console.log('MongoDB Error:', err);
-});
-
-
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
